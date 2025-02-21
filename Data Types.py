649114c93a83528567d3594e0e858191c4d5e855
shreyasipanthee <shreyasipanthee@gmail.com>
2025-02-21 14:21:00 -0500

    Created variables of different data types and printed them.

---
+++ Data Types.py
@@ -0,0 +1,27 @@
+#Author: Shreya Panthee
+#Date Modified:
+#Description:
+
+#Creating a variable to store an integer data type
+integer = int(23)
+
+#Creating a variable to store a string data type
+string = str("Hello")
+
+#Creating a variable to store a boolean data type
+boolean = bool(True)
+
+#Creating a variable to store a float data type
+float = float(2.37)
+
+#This prints an integer
+print("This is an integer " + str(integer))
+
+#This prints a string
+print("This is a string " + string)
+
+#This prints a boolean
+print("This is a boolean " + str(boolean))
+
+#This prints a float
+print("This is a float " + str(float))
