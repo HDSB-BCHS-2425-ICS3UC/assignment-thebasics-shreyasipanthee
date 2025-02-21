3b1f97682622cda1d890ac41843ea6e3daf2e96c
shreyasipanthee <shreyasipanthee@gmail.com>
2025-02-21 14:26:17 -0500

    Wrote code to perform various math operations and print the result.

---
+++ Math Operations.py
@@ -0,0 +1,39 @@
+#Author: Shreya Panthee
+#Date Modified:
+#Description:
+
+#Adds 2 numbers
+sum_add=3+250
+
+#Finds the difference between 3 and -250
+difference = 3-250
+
+#Finds the product of 3 2 and 5
+product = 3*2*5
+
+#finds the quotient
+quotient = 251/6
+
+#finds 2 to the power of 3
+exponent = 2**3
+
+#find the remainder when dividing 27 by 17
+remainder = 27%17
+
+#Prints Sum
+print(sum_add)
+
+#Prints Difference
+print(difference)
+
+#Prints Product
+print(product)
+
+#Prints Quotient
+print(quotient)
+
+#Prints Exponent
+print(exponent)
+
+#Prints Remainder
+print(remainder)
